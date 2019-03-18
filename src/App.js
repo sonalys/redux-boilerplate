a8cee2c80623459286d0bfd8144298d8663358d5
Alysson Carvalho Ribeiro <alysson.ribeiro@raccoon.ag>
2019-03-18 14:16:59 -0300

    Now it updates its value correctly.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-
+import clickButton from './actions/actions';
 
 class App extends Component {
+  updatePlease(){
+   this.props.dispatch(clickButton);
+  console.log("ACTION TRIGGERED");
+  }
+
   render() {
     const { newValue } = this.props;
     return (
       <div className="App">
         <header className="App-header">
-         <p>Hello World! {newValue}.</p>
+         <p onClick={()=> { this.updatePlease(); }}>Hello World! {newValue}.</p>
         </header>
       </div>
     );
@@ -17,6 +22,7 @@ class App extends Component {
 }
 
 const mapStateToProps = store => ({
+  dispatch: message => {store.dispatch(message);},
   newValue: store.testState.newValue
 });
 
---
+++ src/actions/actions.js
@@ -0,0 +1,6 @@
+export const clickButton = {
+    type: "CLICK_BUTTON",
+    newValue: "OH IT CHANGED"
+};
+
+export default clickButton;
